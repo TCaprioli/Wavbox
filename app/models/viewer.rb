d0eeaab2a790b812c8f05ea8d5044c3928a4d4b2
Tcaprioli <Capriolityler@gmail.com>
2020-02-11 18:03:57 -0500

    deleted viewer

--- app/models/viewer.rb
+++
@@ -1,9 +0,0 @@
-class Viewer < ApplicationRecord
-    has_many :subs
-    has_many :content_creators, through: :subs
-
-    has_secure_password
-    validates :password, length: { in: 6..20 }
-    validates :password, confirmation: true,
-        unless: Proc.new { |a| a.password.blank? }
-end
